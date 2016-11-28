c966cb4be58d85756ef45d7e428ffae307a424f8
litterzhang <litterzhang824@gmail.com>
2016-11-28 12:00:53 +0800

    保存

--- chatjsonctl.py
+++ chatjsonctl.py
@@ -11,131 +11,115 @@ import json
 import chatjsondb
 import ChatUtils
 
-name = None
-sim_new = 0.7
-sim_old = 0.2
+class Chat:
+	# 初始化对话
+	def __init__(self, _name, _sim_new=0.7, _sim_old=0.2):
 
-coms = None
+		self.name = _name
+		self.sim_new = _sim_new
+		self.sim_old = _sim_old
 
-# 初始化对话
-def init_chat(_name, _sim_new=0.7, _sim_old=0.2):
-	global name, sim_new, sim_old, coms
+		chatjsondb.init_db(self.name)
 
-	name = _name
-	sim_new = _sim_new
-	sim_old = _sim_old
+		self.coms = list()
 
-	chatjsondb.init_db(name)
+	# 结束对话
+	def __del__(self):
+		COMS = os.path.join(os.path.dirname(__file__), 'coms')
+		with open(os.path.join(COMS, '%s_%s' % (self.name, datetime.datetime.now().strftime("%Y%m%d %H%M%S"))), 'w', encoding='utf-8') as fw:
+			coms_str = json.dumps(self.coms, ensure_ascii=False)
+			fw.write(coms_str)
 
-	coms = list()
-
-# 结束对话
-def stop_chat():
-	global name, sim_new, sim_old, coms
-
-	COMS = os.path.join(os.path.dirname(__file__), 'coms')
-	with open(os.path.join(COMS, '%s_%s' % (name, datetime.datetime.now().strftime("%Y%m%d %H%M%S"))), 'w', encoding='utf-8') as fw:
-		coms_str = json.dumps(coms, ensure_ascii=False)
-		fw.write(coms_str)
-
-# 创建新的问题
-def create_question(que_str, que_type=2):
-	res = chatjsondb.create_question(que_str, que_type=que_type)
-	if res.success:
-		que = res.content.get('que', None)
-		que_id = res.content.get('id', -1)
-
-		if que:
-			que['id'] = que_id
-		return que
-	else:
-		return None
-
-# 匹配问题
-def match_question(que_str):
-	res = chatjsondb.get_answers(que_str)
-
-	que = None
-	sim = -1
-	anss = list()
-	if res.success:
-		que = res.content.get('que', None)
-		sim = res.content.get('sim', -1)
-		anss = res.content.get('anss', list())
-
-	return que, sim, anss
-
-# 进行问题控制
-def chat_ctl_question(que_str, que, sim, anss, que_type=2):
-	global name, sim_new, sim_old, coms
-	
-	que_new = create_question(que_str, que_type)
-
-	if que and sim > sim_old:
-		chatjsondb.match_old_anss(que_new['id'], que['id'])
-	else:
-		anss = list()
-	que = que_new
-	sim = 1
-	return que, sim, anss
-
-# 进行回答控制
-def chat_ctl_answer(que, sim, anss):
-	global name, sim_new, sim_old, coms
-
-	ans = ChatUtils.default_ans_json()
-	score = 0
-	
-	if sim < 0.2:
-		ans = ChatUtils.blur_ans_json()
-	else:
-		# 查找之前相同的问题
-		times = 0
-		last_ans = None
-		for com in coms:
-			if com['que_sim']>=0.2 and que['id']==com['que']['id']:
-				times += 1
-				last_ans = com['ans']
-
-		# 匹配固定的回答
-		if que['type']==0:
-			ans_seed = -1
-			ans_type = -1
-			if last_ans:
-				ans_seed = last_ans['seed']
-				ans_type = last_ans['type']
-			ans, score = ChatUtils.search_ans_json(anss, times=times, ans_seed=ans_seed, ans_type=ans_type)
-		elif que['type']==1:
-			ans, score = ChatUtils.search_ans_json(anss, times=times)
+	# 创建新的问题
+	def create_question(que_str, que_type=2):
+		res = chatjsondb.create_question(que_str, que_type=que_type)
+		if res.success:
+			que = res.content.get('que', None)
+			return que
 		else:
-			ans, score = ChatUtils.search_ans_json(anss, times=times)		
+			return None
 
-	# 记录本次回答
-	coms.append({
-		'que' : que,
-		'que_sim' : sim,
-		'ans' : ans,
-		'ans_score' : score
-	})
+	# 匹配问题
+	def match_question(que_str):
+		res = chatjsondb.get_answers(que_str)
 
-	return ans, score
+		que = None
+		sim = -1
+		anss = list()
+		if res.success:
+			que = res.content.get('que', None)
+			sim = res.content.get('sim', -1)
+			anss = res.content.get('anss', list())
 
-# 创建新的答案
-def create_answer(ans_str, ans_type=2, ans_seed=-1, ans_deg=0):
-	res = chatjsondb.create_answer(ans_str, ans_type, ans_seed, ans_deg)
-	ans_id = -1
-	if res.success:
-		ans_id = res.content.get('id', -1)
-	return ans_id
+		return que, sim, anss
 
-# 为问题创建答案
-def add_answer(que_id, ans_str, ans_type=2, ans_seed=-1, ans_deg=0, score=10):
-	res = chatjsondb.add_ans_for_que(que_id, ans_str, ans_type, ans_seed, ans_deg, score)
-	return res.success
+	# 对话添加
+	def coms_add_que(self, que_str, que, sim):
+		self.coms.append({'que_str' : que_str, 'que' : que, 'que_sim' : sim})
 
-# 更改答案打分
-def change_score(que_id, ans_id, score=0):
-	chatjsondb.update_ans2que(que_id, ans_id, score)
+	def coms_add_ans(self, ans, score):
+		self.coms[-1]['ans'] = ans
+		self.coms[-1]['score'] = score
+
+	# 进行问题控制
+	def chat_ctl_question(self, que_str, que, sim, anss, que_type=2):
+		que_new = Chat.create_question(que_str, que_type)
+
+		if que and sim > self.sim_old:
+			chatjsondb.match_old_anss(que_new['id'], que['id'])
+		else:
+			anss = list()
+		que = que_new
+		sim = 1
+		return que, sim, anss
+
+	# 进行回答控制
+	def chat_ctl_answer(self, que, sim, anss):
+		ans = ChatUtils.default_ans_json()
+		score = 0
+		
+		if sim < 0.2:
+			ans = ChatUtils.blur_ans_json()
+		else:
+			# 查找之前相同的问题
+			times = 0
+			last_ans = None
+			for com in self.coms:
+				if com['que_sim']>=0.2 and que['id']==com['que']['id']:
+					last_ans = com.get('ans', None)
+					if last_ans:
+						times += 1
+
+			# 匹配固定的回答
+			if que['type']==0:
+				ans_seed = -1
+				ans_type = -1
+				if last_ans:
+					ans_seed = last_ans['seed']
+					ans_type = last_ans['type']
+				ans, score = ChatUtils.search_ans_json(anss, times=times, ans_seed=ans_seed, ans_type=ans_type)
+			elif que['type']==1:
+				ans, score = ChatUtils.search_ans_json(anss, times=times)
+			else:
+				ans, score = ChatUtils.search_ans_json(anss, times=times)
+		return ans, score
+
+	# 创建新的答案
+	def create_answer(ans_str, ans_type=2, ans_seed=-1, ans_deg=0):
+		res = chatjsondb.create_answer(ans_str, ans_type, ans_seed, ans_deg)
+		ans_id = -1
+		if res.success:
+			ans_id = res.content.get('id', -1)
+		return ans_id
+
+	# 为问题创建答案
+	def add_answer(que_id, ans_str, ans_type=2, ans_seed=-1, ans_deg=0, score=10):
+		res = chatjsondb.add_ans_for_que(que_id, ans_str, ans_type, ans_seed, ans_deg, score)
+		return res.success
+
+	# 更改答案打分
+	def change_score(que_id, ans_id, score=0):
+		chatjsondb.update_ans2que(que_id, ans_id, score)
 
 # 输入int值
 def input_int(input_tip='', default=0):
@@ -167,7 +151,7 @@ def create_anss(que_id):
 		ans_deg = input_int(input_tip='答案语气 : ', default=0)
 		ans_score = input_int(input_tip='答案打分 : ', default=10)
 		
-		add_answer(que_id, ans_str, ans_type, ans_seed, ans_deg, ans_score)
+		Chat.add_answer(que_id, ans_str, ans_type, ans_seed, ans_deg, ans_score)
 
 # 训练模式
 def chat_ex():
@@ -179,7 +163,7 @@ def chat_ex():
 	sim_old = input_float('旧问题阈值sim_old ? ', default=0.2)
 	
 	# 加载问答库	
-	init_chat(man_name, sim_new, sim_old)
+	chat = Chat(man_name, sim_new, sim_old)
 
 	print('----------开始问答----------\n')
 
@@ -188,18 +172,20 @@ def chat_ex():
 		que_str = input('--------------------\nMr. %s: ' % man_name)
 		
 		if que_str=='end':
-			stop_chat()
 			break
 
 		# 进行问题匹配
-		que, sim, anss = match_question(que_str)
+		que, sim, anss = Chat.match_question(que_str)
+
 		if not que or sim < sim_new:
 			que_new_type = input_int(input_tip='\n这是一个新问题, 输入问题类型: ', default=2)
+			que, sim, anss = chat.chat_ctl_question(que_str, que, sim, anss, que_new_type)
 
-			que, sim, anss = chat_ctl_question(que_str, que, sim, anss, que_new_type)
+		chat.coms_add_que(que_str, que, sim)
 
 		#这里进行答案匹配
-		ans, score = chat_ctl_answer(que, sim, anss)
+		ans, score = chat.chat_ctl_answer(que, sim, anss)
+		chat.coms_add_ans(ans, score)
 
 		print('Mr. Bot: %s\n' % ans['content'])
 		print('Answer type : %s\nAnswer seed : %s\nAnswer deg : %s\nAnswer score : %s\n' % (ans['type'], ans['seed'], ans['deg'], score))
@@ -208,7 +194,7 @@ def chat_ex():
 			create_anss(que['id'])
 		else:
 			ans_score = input_int(input_tip='给答案打分 : ', default=0)
-			change_score(que['id'], ans['id'], ans_score)
+			Chat.change_score(que['id'], ans['id'], ans_score)
 			
 			create_anss(que['id'])
 
@@ -218,7 +204,7 @@ def chat():
 	man_name = input('What\'s your name ? ')
 	
 	# 加载问答库	
-	init_chat(man_name)
+	chat = Chat(man_name)
 
 	print('----------开始问答----------\n')
 
@@ -227,16 +213,15 @@ def chat():
 		que_str = input('--------------------\nMr. %s: ' % man_name)
 		
 		if que_str=='end':
-			stop_chat()
 			break
 		# 进行问题匹配
-		que, sim, anss = match_question(que_str)
+		que, sim, anss = Chat.match_question(que_str)
 		#这里进行答案匹配
-		ans, score = chat_ctl_answer(que, sim, anss)
+		ans, score = chat.chat_ctl_answer(que, sim, anss)
 
 		print('Mr. Bot: %s\n' % ans['content'])
 		
 
 if __name__=='__main__':
-	chat()
+	chat_ex()
 	
--- web/app.py
+++ web/app.py
@@ -12,39 +12,48 @@ from settings import *
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
-import chatdb
+from chatjsonctl import Chat
 
 import functools
 
+chats = dict()
+
 class BaseHandler(tornado.web.RequestHandler):
-	def initialize(self):
-		self.name = None
-		self.sim_new = sim_new_defalut
-		self.sim_old = sim_old_defalut
+	@property
+	def name(self):
+		name_cookie = self.get_secure_cookie('name')
+		name = None
+		if name_cookie:
+			name = bytes.decode(self.get_secure_cookie('name'))
+		return name
+
+	@property
+	def chat(self):
+		global chats
+
+		chat = chats.get(self.name, None)
+		if chat:
+			chat = chat['chat']
+		return chat
 
 def check_login(func):
 	@functools.wraps(func)
 	def wrapper(self):
-		if not self.get_secure_cookie('name'):
+		if not self.name or not self.chat:
 			self.redirect('/login')
 			return
-		else:
-			self.name = self.get_secure_cookie('name')
-			if self.get_secure_cookie('sim_new'):
-				self.sim_new = tofloat(self.get_secure_cookie('sim_new'), default=sim_new_defalut)
-			if self.get_secure_cookie('sim_old'):
-				self.sim_old = tofloat(self.get_secure_cookie('sim_old'), default=sim_old_defalut)
 		return func(self)
 	return wrapper
 
 def check_not_login(func):
 	@functools.wraps(func)
 	def wrapper(self):
-		if self.name or self.get_secure_cookie('name'):
+		if self.name:
 			self.redirect('/')
 			return
 		return func(self)
 	return wrapper
+
 
 def tofloat(float_str, default=0):
 	res = default
@@ -54,10 +63,20 @@ def tofloat(float_str, default=0):
 		pass
 	return res
 
+def toint(int_str, default=0):
+	res = default
+	try:
+		res = int(int_str)
+	except:
+		pass
+	return res
+
 class MainHandler(BaseHandler):
 	@check_login
 	def get(self):
-		self.render('index.html', name=self.name, sim_new=self.sim_new, sim_old=self.sim_old)
+		global chats
+
+		self.render('que_ask.html', chat=self.chat)
 
 class LoginHandler(BaseHandler):
 	@check_not_login
@@ -66,42 +85,91 @@ class LoginHandler(BaseHandler):
 
 	@check_not_login
 	def post(self):
+		global chats
+
 		if self.get_argument('name'):
 			self.set_secure_cookie('name', self.get_argument('name'))
-			self.name = self.get_argument('name')
-		if self.get_argument('sim_new'):
-			self.set_secure_cookie('sim_new', self.get_argument('sim_new'))
-			self.sim_new = tofloat(self.get_argument('sim_new'), default=sim_new_defalut)
-		if self.get_argument('sim_old'):
-			self.set_secure_cookie('sim_old', self.get_argument('sim_old'))
-			self.sim_old = tofloat(self.get_argument('sim_old'), default=sim_old_defalut)
 
-		chatdb.init_db(self.name)
-		self.redirect('/')
+			name = self.get_argument('name')
+			sim_new = tofloat(self.get_argument('sim_new'), default=sim_new_defalut)
+			sim_old = tofloat(self.get_argument('sim_old'), default=sim_old_defalut)
+			chats[name] = {'name' : name, 'chat' : Chat(name, sim_new, sim_old)}
+
+			self.redirect('/')
+		else:
+			self.redirect('/login')
 
 class LogoutHandler(BaseHandler):
 	@check_login
 	def get(self):
-		self.name = None
-		self.sim_new = sim_new_defalut
-		self.sim_old = sim_old_defalut
-		self.set_secure_cookie('name', '')
-		self.set_secure_cookie('sim_new', '')
-		self.set_secure_cookie('sim_old', '')
+		global chats
 
+		del chats[self.name]['chat']
+		del chats[self.name]
+
+		self.set_secure_cookie('name', '')
+		
 		self.redirect('/login')
 
-class QuestionHandler(BaseHandler):
+class QuestionAskHandler(BaseHandler):
 	@check_login
 	def post(self):
-		
-		
+		if self.get_argument('que_content'):
+			que, sim, anss = Chat.match_question(self.get_argument('que_content'))
+			if not que or sim < self.chat.sim_new:
+				self.render('que_add.html', chat=self.chat, que_content=self.get_argument('que_content'))
+			else:
+				self.chat.coms_add_que(self.get_argument('que_content'), que, sim)
+				ans, score = self.chat.chat_ctl_answer(que, sim, anss)
+				self.chat.coms_add_ans(ans, score)
+
+				self.render('ans_add.html', chat=self.chat, que_id=que['id'])
+		else:
+			self.redirect('/')
+
+class QuestionAddHandler(BaseHandler):
+	@check_login
+	def post(self):
+		if self.get_argument('que_content'):
+			que_type = toint(self.get_argument('que_type'), default=2)
+			que, sim, anss = Chat.match_question(self.get_argument('que_content'))
+			que, sim, anss = self.chat.chat_ctl_question(self.get_argument('que_content'), que, sim, anss, que_type)
+			self.chat.coms_add_que(self.get_argument('que_content'), que, sim)
+			ans, score = self.chat.chat_ctl_answer(que, sim, anss)
+			self.chat.coms_add_ans(ans, score)
+
+			print(que)
+			self.render('ans_add.html', chat=self.chat, que_id=que['id'])
+		else:
+			self.redirect('/')
+
+class AnswerAddHandler(BaseHandler):
+	@check_login
+	def post(self):
+		if self.get_argument('que_id') and self.get_argument('ans_content'):
+			que_id = toint(self.get_argument('que_id'))
+			ans_content = self.get_argument('ans_content')
+			ans_type = toint(self.get_argument('ans_type'), default=2)
+			# ans_seed = toint(self.get_argument('ans_seed'), default=-1)
+			ans_seed = -1
+			ans_deg = toint(self.get_argument('ans_deg'), default=0)
+			ans_score = toint(self.get_argument('ans_score'), default=10)
+
+			Chat.add_answer(que_id, ans_content, ans_type, ans_seed, ans_deg, ans_score)
+			print(que_id)
+			
+			self.render('ans_add.html', chat=self.chat, que_id=que_id)
+		else:
+			self.redirect('/')
+
 if __name__=='__main__':
     application = tornado.web.Application([
         (r'/', MainHandler),
         (r'/login', LoginHandler),
         (r'/logout', LogoutHandler),
-        (r'/question', QuestionHandler)
+        (r'/que_ask', QuestionAskHandler),
+        (r'/que_add', QuestionAddHandler),
+        (r'/ans_add', AnswerAddHandler),
     ], **app_settings)
 
     application.listen(3000)
